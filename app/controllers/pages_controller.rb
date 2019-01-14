2a1dbd1844abdc2bfc6085039802547eeeeb3b64
vanisree <vimala.vani123@gmail.com>
2019-01-14 13:18:45 +0530

    pages controller added

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+  get 'pages/new'
+  get 'pages/show'
+  get 'pages/edit'
+  get 'pages/update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
